e8945508704c6968e48ee04aa933d1a367448cd4
Cerne17 <miguelbadanycerne.20231@poli.ufrj.br>
2024-03-18 18:55:32 -0300

    chore(filestruct): Fixed the register file structure

---
+++ DjangoStockChecker/register/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render, HttpResponse
+
+def register(request):
+    """
+        Register
+        Página de Registro
+    """
+    return HttpResponse("Hello, World. You're at the Register page.")
